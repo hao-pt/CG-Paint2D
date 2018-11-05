ce4ea68db0e99b89a13ec8c047a0ffa6bf3682bd
Hao Phung <tienhaophung@gmail.com>
2018-11-05 19:48:06 +0700

    Hao - Upload project

---
+++ SharpGL/SharpGL/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SharpGL
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openGLControl = new SharpGL.OpenGLControl();
+			this.bt_Line = new System.Windows.Forms.Button();
+			this.bt_Circle = new System.Windows.Forms.Button();
+			this.bt_Palette = new System.Windows.Forms.Button();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// openGLControl
+			// 
+			this.openGLControl.DrawFPS = false;
+			this.openGLControl.Location = new System.Drawing.Point(0, 41);
+			this.openGLControl.Name = "openGLControl";
+			this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+			this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+			this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+			this.openGLControl.Size = new System.Drawing.Size(751, 374);
+			this.openGLControl.TabIndex = 0;
+			this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+			this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+			this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+			this.openGLControl.Load += new System.EventHandler(this.openGLControl1_Load);
+			this.openGLControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ctrl_OpenGLControl_MouseDown);
+			this.openGLControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ctrl_OpenGLControl_MouseUp);
+			// 
+			// bt_Line
+			// 
+			this.bt_Line.Location = new System.Drawing.Point(12, 9);
+			this.bt_Line.Name = "bt_Line";
+			this.bt_Line.Size = new System.Drawing.Size(75, 23);
+			this.bt_Line.TabIndex = 1;
+			this.bt_Line.Text = "Line";
+			this.bt_Line.UseVisualStyleBackColor = true;
+			this.bt_Line.Click += new System.EventHandler(this.bt_Line_Click);
+			// 
+			// bt_Circle
+			// 
+			this.bt_Circle.Location = new System.Drawing.Point(93, 9);
+			this.bt_Circle.Name = "bt_Circle";
+			this.bt_Circle.Size = new System.Drawing.Size(75, 23);
+			this.bt_Circle.TabIndex = 2;
+			this.bt_Circle.Text = "Circle";
+			this.bt_Circle.UseVisualStyleBackColor = true;
+			// 
+			// bt_Palette
+			// 
+			this.bt_Palette.Location = new System.Drawing.Point(664, 9);
+			this.bt_Palette.Name = "bt_Palette";
+			this.bt_Palette.Size = new System.Drawing.Size(75, 23);
+			this.bt_Palette.TabIndex = 3;
+			this.bt_Palette.Text = "Palette";
+			this.bt_Palette.UseVisualStyleBackColor = true;
+			this.bt_Palette.Click += new System.EventHandler(this.bt_Palette_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(751, 415);
+			this.Controls.Add(this.bt_Palette);
+			this.Controls.Add(this.bt_Circle);
+			this.Controls.Add(this.bt_Line);
+			this.Controls.Add(this.openGLControl);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private OpenGLControl openGLControl;
+		private System.Windows.Forms.Button bt_Line;
+		private System.Windows.Forms.Button bt_Circle;
+		private System.Windows.Forms.Button bt_Palette;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+	}
+}
+
---
+++ SharpGL/SharpGL/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpGL
+{
+	public partial class Form1 : Form
+	{
+		Color colorUserColor; // Bien mau de ve hinh
+		short shShape; // 0 neu muon ve duong thang, 1 neu duong tron, ...
+
+		Point pStart, pEnd; // Toa do diem dau va diem cuoi
+							// Point thuoc lop System.Drawing
+
+		public Form1()
+		{
+			InitializeComponent();
+			colorUserColor = Color.White; // Gia tri mac dinh la mau trang
+			shShape = 0; // Mac dinh ve duong thang
+		}
+
+		private void openGLControl1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void bt_Palette_Click(object sender, EventArgs e)
+		{
+			// Goi hop thoai chon mau
+			if (colorDialog1.ShowDialog() == DialogResult.OK)
+				colorUserColor = colorDialog1.Color; // Luu lai mau user chon
+		}
+
+		// Nguoi dung chon chuc nang ve duong thang
+		private void bt_Line_Click(object sender, EventArgs e)
+		{
+			shShape = 0; // Nguoi dung chon ve duong thang
+		}
+
+		// Cap nhat toa do diem cuoi khi nguoi dung keo chuot di
+		private void ctrl_OpenGLControl_MouseUp(object sender, MouseEventArgs e)
+		{
+			pEnd = e.Location; // Lay toa do diem cuoi
+		}
+
+		private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+		{
+			// get the openGL object
+			OpenGL gl = openGLControl.OpenGL;
+			// set the clear color: dat mau nen
+			// alpha: do trong suot
+			gl.ClearColor(0, 0, 0, 0);
+			// set the projection matrix
+			// Xet ma tran phep chieu
+			// 2D: chỉ quan tam projection matrix
+			gl.MatrixMode(OpenGL.GL_PROJECTION);
+			// load the identify
+			// Xét ma trận hiện hành là ma trận đơn vị
+			gl.LoadIdentity();
+		}
+
+		// Ham ve doan thang
+		private void drawLine(OpenGL gl) {
+			gl.Begin(OpenGL.GL_LINES);
+			gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+			gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pEnd.Y);
+			gl.End();
+			gl.Flush();
+		}
+
+		// Cac ham ve khac ...
+
+		private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
+		{
+			// get the OpenGL object
+			OpenGL gl = openGLControl.OpenGL;
+			// clear the color and depth buffer
+			// Xóa màn hình
+			gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+			// Chon mau
+			gl.Color(colorUserColor.R / 255.0, colorUserColor.G / 255.0, colorUserColor.B / 255.0, 0);
+
+			// Ve voi cho nay
+			// ...
+			switch (shShape)
+			{
+				case 0:
+					// Ve doan thang
+					drawLine(gl);
+					break;
+				case 1:
+					// Ve duong tron
+
+					break;
+				// case 2..n
+			}
+
+			#region ViDuVeTamGiac
+			/* 
+			
+			gl.Begin(OpenGL.GL_TRIANGLES); // Ve tam giac
+			gl.Vertex2sv(new short[] { 0, 0 }); // Dinh A(0, 0)
+			gl.Vertex2sv(new short[] { 200, 200 }); // Dinh B(100, 100)
+			gl.Vertex2sv(new short[] { 500, 0 }); // Dinh C(200, 0)
+			gl.End(); // Kết thúc
+			gl.Flush(); // Thuc hien ve ngay thay vi phai doi sau 1 thoi gian
+						// Bản chất khi vẽ thì nó vẽ lên vùng nhớ Buffer
+						// Do đó cần dùng hàm Flush để đẩy vùng nhớ Buffer này lên màn hình
+			*/
+			#endregion
+		}
+
+
+		private void openGLControl_Resized(object sender, EventArgs e)
+		{
+			// get the OpenGL object
+			OpenGL gl = openGLControl.OpenGL;
+			// set the projection matrix
+			gl.MatrixMode(OpenGL.GL_PROJECTION);
+			// load the identify
+			gl.LoadIdentity();
+			// Create a perspective transformation
+			gl.Viewport(0, 0, openGLControl.Width, openGLControl.Height); // Xét cái màn hình: Vẽ toàn bộ cái khung của OpenGL control
+
+			// Hàm set up cái phép chiếu trực giao
+			// Ở đây chính là cái size của khung OpenGL control
+			gl.Ortho2D(0, openGLControl.Width, 0, openGLControl.Height);
+		}
+
+		// Cap nhat diem dau
+		private void ctrl_OpenGLControl_MouseDown(object sender, MouseEventArgs e)
+		{
+			// Cap nhat toa do diem dau
+			pStart = e.Location; // e la tham so lien quan den su kien chon diem
+			pEnd = pStart; // Mac dinh pEnd = pStart
+		}
+
+	}
+}
